d2003ba07f8afb55fd2da5265428702614e754a8
epresaa <epresa@birt.eus>
2023-11-16 15:38:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,37 +17,37 @@
             <h1>Formulario de alquiler: </h1><br><label>Nombre</label><br>
             
             <!-- Nombre -->
-            <input type="textarea" name="nombre" id="nombre">
+            <input type="textarea" name="nombre" id="nombre" required>
             <br><br><br>
 
             <!-- Apellidos -->
             <label>Apellidos</label><br>
-            <input type="textarea" name="apellidos" id="apellidos">
+            <input type="textarea" name="apellidos" id="apellidos" required>
             <br><br><br>
 
             <!-- Título del libro -->
             <label>Libro</label><br>
-            <input type="textarea" name="libro" id="libro">
+            <input type="textarea" name="libro" id="libro" required>
             <br><br><br>
 
             <!-- Email -->
             <label>Email</label><br>
-            <input type="textarea" name="email" id="email">
+            <input type="textarea" name="email" id="email" required>
             <br><br><br>
 
             <!-- Teléfono -->
             <label>Teléfono</label><br>
-            <input type="textarea" name="tel" id="tel">
+            <input type="textarea" name="tel" id="tel" required>
             <br><br><br>       
             
             <!-- Fecha -->
             <label>Fecha de Alquiler</label><br>
-            <input type="date" name="fecha" id="fecha">
+            <input type="date" name="fecha" id="fecha" required>
             <br><br><br>
 
             <!-- DNI -->
             <label>DNI</label><br>
-            <input type="textarea" name="dni" id="dni">
+            <input type="textarea" name="dni" id="dni" required>
             <br><br>
             
             <input type="submit" value="Enviar" id="boton"/>
